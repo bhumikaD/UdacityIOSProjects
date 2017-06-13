56141968af028b065f06655bc13cdf3a2017abc9
bhumika dwivedi <bhumikadwivedi@Virendras-Mac-mini.local>
2017-06-13 21:16:10 +0530

    removed missing files warnings

---
+++ FunnyLyricsProject/FunnyLyrics/FunnyLyrics/FunnyLyrics-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FunnyLyrics-Bridging-Header.h
+//  FunnyLyrics
+//
+//  Created by bhumika dwivedi on 6/13/17.
+//  Copyright © 2017 bhumika dwivedi. All rights reserved.
+//
+
+#ifndef FunnyLyrics_Bridging_Header_h
+#define FunnyLyrics_Bridging_Header_h
+
+
+#endif /* FunnyLyrics_Bridging_Header_h */
